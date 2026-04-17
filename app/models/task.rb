826bcfa558107d4ab4bbfec730342402dc2a664c
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-04-17 19:51:48 +0900

    chore: annotateの設定をインストール/アノテーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  completed  :boolean          default(FALSE), not null
+#  content    :text
+#  deadline   :datetime
+#  title      :string(100)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
 end
