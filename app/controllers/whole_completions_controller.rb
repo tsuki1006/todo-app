041a5061548c91f8bb9ae61be4dc856598508ce3
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-04-26 12:25:03 +0900

    fix: 完了状態の一括変更時、フィルターを維持するよう修正

--- app/controllers/whole_completions_controller.rb
+++ app/controllers/whole_completions_controller.rb
@@ -1,13 +1,15 @@
 class WholeCompletionsController < ApplicationController
   def create
+    type = params[:type] if params[:type].present?
     uncompleted_tasks = Task.uncompleted
     uncompleted_tasks.update_all(completed: true)
-    redirect_to root_path
+    redirect_to root_path(type: type)
   end
 
   def destroy
+    type = params[:type] if params[:type].present?
     completed_tasks = Task.completed
     completed_tasks.update_all(completed: false)
-    redirect_to root_path
+    redirect_to root_path(type: type)
   end
 end
